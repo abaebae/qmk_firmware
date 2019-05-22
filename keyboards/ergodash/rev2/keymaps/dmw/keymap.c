bc25656862e80123143900758cc3e37925b56748
Dominic Wein <dowein@gmail.com>
2019-05-22 19:34:59 +0200

    added numlock to layer 1

--- keyboards/ergodash/rev2/keymaps/dmw/keymap.c
+++ keyboards/ergodash/rev2/keymaps/dmw/keymap.c
@@ -16,7 +16,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                  ),
    [1] = LAYOUT( \
                 KC_ESC,       KC_F1,      KC_F2,      KC_F3,         KC_F4,      KC_F5,      KC_F6,                  KC_F7,      KC_F8,      KC_F9,      KC_F10,     KC_F11,     KC_F12,     KC_PSCR, \
-                KC_TAB,       KC_NO,      KC_NO,      KC_END,        KC_NO,      KC_NO,      KC_LBRC,                KC_RBRC,    KC_PSLS,    KC_P7,      KC_P8,      KC_P9,      KC_UP,      KC_NO,   \
+                KC_TAB,       KC_NO,      KC_NO,      KC_END,        KC_NO,      KC_NO,      KC_LBRC,                KC_RBRC,    KC_PSLS,    KC_P7,      KC_P8,      KC_P9,      KC_UP,      KC_NLCK, \
                 KC_NO,        KC_HOME,    KC_NO,      KC_NO,         KC_RGHT,    KC_NO,      KC_NO,                  KC_NO,      KC_PAST,    KC_P4,      KC_P5,      KC_P6,      KC_PPLS,    KC_NO,   \
                 KC_LSFT,      KC_NO,      KC_NO,      KC_NO,         KC_NO,      KC_LEFT,    KC_NO,                  KC_NO,      KC_DOWN,    KC_P1,      KC_P2,      KC_P3,      KC_PMNS,    KC_RSFT, \
                 KC_LCTL,      KC_LGUI,    KC_NO,      KC_TRNS,       KC_LALT,    KC_SPC,     KC_NO,                  KC_NO,      KC_ENT,     KC_P0,      KC_PDOT,    KC_NO,      KC_EQL,     KC_RCTL  \
